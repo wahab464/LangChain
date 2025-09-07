a824682ffaa90a821ef2dec7f0509384f037126d
MUHAMMAD ABDUL WAHAB <151780468+wahab464@users.noreply.github.com>
2025-09-07 20:32:38 +0500

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+chat_history = []
+
+while True:
+    user_input = input('You : ')
+    chat_history.append(user_input)
+    if user_input == 'exit':
+        break
+    result = model.invoke(user_input)
+    chat_history.append(result.content)
+    print("AI : ",result.content)
+
+print(chat_history)
